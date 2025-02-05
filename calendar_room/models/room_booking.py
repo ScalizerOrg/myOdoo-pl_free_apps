107d41c9125910e0e439d15e0c18fd5e3ea83052
Khalid BENTALEB <khalid@scalizer.fr>
2025-02-05 15:38:21 +0100

    [MIG][18.0] project_forecast_task_planning

--- calendar_room/models/room_booking.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class RoomBooking(models.Model):
-    _inherit = "room.booking"
-
-    event_id = fields.One2many("calendar.event", "booking_id")
-    active = fields.Boolean("Active", related="event_id.active", tracking=True)
